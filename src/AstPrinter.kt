48c9029e846f08a059eb3d2ea7b55889428ce796
Adam Vandolder <adam.vandolder@gmail.com>
2019-12-02 21:25:47 -0500

    Switch AstPrinter to when match instead of visitor

--- src/AstPrinter.kt
+++ src/AstPrinter.kt
@@ -1,39 +1,20 @@
 package com.craftinginterpreters.lox
 
-class AstPrinter : Expr.Visitor<String> {
-    fun print(expr: Expr) = expr.accept(this)
-
-    override fun visitBinaryExpr(expr: Expr.Binary): String =
-            parenthesize(expr.operator.lexeme, expr.left, expr.right)
-
-    override fun visitGroupingExpr(expr: Expr.Grouping): String =
-            parenthesize("group", expr.expression)
-
-    override fun visitLiteralExpr(expr: Expr.Literal): String =
-            if (expr.value == null) "nil"
-            else expr.value.toString()
-
-    override fun visitUnaryExpr(expr: Expr.Unary): String =
-            parenthesize(expr.operator.lexeme, expr.right)
-
-    override fun visitTernaryExpr(expr: Expr.Ternary): String =
-            parenthesize("ternary", expr.condition, expr.left, expr.right)
-
-    private fun parenthesize(name: String, vararg exprs: Expr): String {
-        val builder = StringBuilder()
-        builder.append("($name")
-        for (expr in exprs) {
-            builder.append(" ${expr.accept(this)}")
-        }
-        builder.append(")")
-        return builder.toString()
-    }
+fun printAst(expr: Expr) = when (expr) {
+    is Expr.Binary -> parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    is Expr.Grouping -> parenthesize("group", expr.expression)
+    is Expr.Literal -> if (expr.value == null) "nil" else expr.value.toString()
+    is Expr.Unary -> parenthesize(expr.operator.lexeme, expr.right)
+    is Expr.Ternary -> parenthesize("ternary", expr.condition, expr.left, expr.right)
 }
 
-fun convertToRpn(expr: Expr): String = when (expr) {
-    is Expr.Binary -> "${convertToRpn(expr.left)} ${convertToRpn(expr.right)} ${expr.operator.lexeme}"
-    is Expr.Grouping -> convertToRpn(expr.expression)
+private fun parenthesize(name: String, vararg exprs: Expr): String =
+        "($name ${exprs.map(::printAst).joinToString(" ")})"
+
+fun printRpn(expr: Expr): String = when (expr) {
+    is Expr.Binary -> "${printRpn(expr.left)} ${printRpn(expr.right)} ${expr.operator.lexeme}"
+    is Expr.Grouping -> printRpn(expr.expression)
     is Expr.Literal -> if (expr.value == null) "null" else expr.value.toString()
-    is Expr.Unary -> "${convertToRpn(expr.right)} ${expr.operator.lexeme}"
-    is Expr.Ternary -> ""
+    is Expr.Unary -> "${printRpn(expr.right)} ${expr.operator.lexeme}"
+    else -> ""
 }
